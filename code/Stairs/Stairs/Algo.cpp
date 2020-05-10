89f5fb0f7377bd331df1c79018737c7d19b02539
Mouad MOUNACH <mounach@gmail.com>
2020-05-10 06:16:20 +0200

    algo file modifs

--- code/Stairs/Stairs/Algo.cpp
+++ code/Stairs/Stairs/Algo.cpp
@@ -5,59 +5,83 @@
 using namespace cv;
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
+    //-----------------get image file-------------------
+    CommandLineParser parser(argc, argv, "{@input | stairs_0.jpg | input image}");
+    Mat img = imread(samples::findFile(parser.get<String>("@input")));
+    
+    if (img.empty())
+    {
+        cout << "Could not open or find the image!\n" << endl;
+        cout << "Usage: " << argv[0] << " <Input image>" << endl;
+        return -1;
+    }
 
-    Mat img = imread("stairs_5.jpg");
-    imshow("Source", img);
 
-    //Apply Gaussian blur to get good results
+    imshow("Original", img);
+
+    //--------------Apply Gaussian blur to get good results-------------
     GaussianBlur(img, img, Size(5, 5), 0, 0);
 
-    Mat dst, out_img, control;
-    Canny(img, dst, 80, 240, 3);
-    cvtColor(dst, out_img, cv::COLOR_GRAY2BGR);
-    cvtColor(dst, control, cv::COLOR_GRAY2BGR);
+    imshow("Gaussian blur", img);
 
-    vector<int> y_keeper_for_lines;
+    Mat canny_img, res_img, Hough_img;
+
+    //--------------Apply Canny filter to detect the edges--------------
+    Canny(img, canny_img, 80, 240, 3);
+
+    cvtColor(canny_img, res_img, cv::COLOR_GRAY2BGR);
+    cvtColor(canny_img, Hough_img, cv::COLOR_GRAY2BGR);
+
+    imshow("Canny filter", canny_img);
+
+    vector<int> lines_y;
     vector<Vec4i> lines;
-    HoughLinesP(dst, lines, 1, CV_PI / 180, 30, 40, 5);
+
+    //--------------Apply Hough filter to detect the lines-------------- 
+    HoughLinesP(canny_img, lines, 1, CV_PI / 180, 30, 40, 5);
 
     for (size_t i = 1; i < lines.size(); i++)
     {
         Vec4i l = lines[i];
-        line(control, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, cv::LINE_AA);
+        line(Hough_img, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, cv::LINE_AA);
     }
 
-    Vec4i l = lines[0];
-    line(out_img, Point(0, l[1]), Point(img.cols, l[1]), Scalar(0, 0, 255), 3, cv::LINE_AA);
-    y_keeper_for_lines.push_back(l[1]);
+    imshow("Hough filter", Hough_img);
 
-    int okey = 1;
+
+    //--------------algorithm approach--------------
+    Vec4i l = lines[0];
+    line(res_img, Point(0, l[1]), Point(img.cols, l[1]), Scalar(0, 0, 255), 3, cv::LINE_AA);
+    lines_y.push_back(l[1]);
+
+    bool check = true;
     int stair_counter = 1;
 
     for (size_t i = 1; i < lines.size(); i++)
     {
         Vec4i l = lines[i];
-        for (int m : y_keeper_for_lines)
+        for (int m : lines_y)
         {
-            if (abs(m - l[1]) < 35)
-                okey = 0;
+            if (abs(m - l[1]) < 25)
+                check = false;
 
         }
-        if (okey)
+        if (check == true)
         {
-            line(out_img, Point(0, l[1]), Point(img.cols, l[1]), Scalar(0, 0, 255), 3, cv::LINE_AA);
-            y_keeper_for_lines.push_back(l[1]);
+            line(res_img, Point(0, l[1]), Point(img.cols, l[1]), Scalar(0, 0, 255), 3, cv::LINE_AA);
+            lines_y.push_back(l[1]);
             stair_counter++;
         }
-        okey = 1;
+        check = true;
 
     }
-    putText(out_img, "Stair number:" + to_string(stair_counter), Point(40, 60), FONT_HERSHEY_SIMPLEX, 1.5, Scalar(0, 255, 0), 2);
-    imshow("Before", img);
-    imshow("Control", control);
-    imshow("detected lines", out_img);
+
+    putText(res_img, "stairs count:" + to_string(stair_counter), Point(40, 60), FONT_HERSHEY_SIMPLEX, 1.5, Scalar(0, 255, 0), 2);
+    
+    imshow("detected lines", res_img);
+    
     waitKey(0);
     return 0;
 }
